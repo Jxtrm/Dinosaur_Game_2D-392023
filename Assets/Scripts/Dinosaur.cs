232087a1302209c0980db3862b370c390a10b859
Jxtrm <j.ecijar@gmail.com>
2023-09-03 19:18:27 +0200

    Player animation, Scroll assets, Spawn obstacles and GameOver Layer
    Se animaron los estados de saltar y correr del jugador, se creo el scroll infinito del mapa junto con la generación aleatoria de obstáculos y la pantalla de GameOver

--- Assets/Scripts/Dinosaur.cs
+++ Assets/Scripts/Dinosaur.cs
@@ -5,25 +5,28 @@ using UnityEngine;
 public class Dinosaur : MonoBehaviour
 {
 
-    private Rigidbody2D rbDinosaur;
     [SerializeField] private float dinoJump;
     [SerializeField] private Transform cCenter;
     [SerializeField] private float cRadius;
     [SerializeField] private LayerMask ground;
-
+    private Rigidbody2D rbDinosaur;
+    private Animator dAnimator;
 
     // Start is called before the first frame update
     void Start()
     {
         rbDinosaur = GetComponent<Rigidbody2D>();
+        dAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        bool isGrounded= Physics2D.OverlapCircle(cCenter.position, cRadius, ground);
+        dAnimator.SetBool("IsGrounded", isGrounded);
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            bool isGrounded= Physics2D.OverlapCircle(cCenter.position, cRadius, ground);
 
             if (isGrounded)
             {
