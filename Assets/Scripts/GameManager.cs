16e0ec38fdb9a8dcc7c89b7f7e6a37862092f081
Jxtrm <j.ecijar@gmail.com>
2023-09-03 21:10:46 +0200

    Increase Scroll Speed
    Se aumenta la velocidad del fondo según pasa el tiempo para darle más dificultad

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,8 +8,10 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private GameObject gameOverScreen;
     [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private float initialScrollSpeed;
     private int score;
     private float timer;
+    private float scrollSpeed;
 
     public static GameManager Instance { get; private set; }
     // Start is called before the first frame update
@@ -29,6 +31,7 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         UpdateScore();
+        UpdateSpeed();
     }
 
     public void ShowGameOverScreen()
@@ -48,5 +51,16 @@ public class GameManager : MonoBehaviour
         timer += Time.deltaTime;
         score = (int)(timer * scorePerSeconds);
         scoreText.text = string.Format("{0:00000}", score);
+    }
+
+    public float GetScrollSpeed()
+    {
+        return scrollSpeed;
+    }
+
+    private void UpdateSpeed()
+    {
+        float speedDivider = 10f;
+        scrollSpeed = initialScrollSpeed + timer / speedDivider;
     }
 }
--- Assets/Scripts/Scroll.cs
+++ Assets/Scripts/Scroll.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 
 public class Scroll : MonoBehaviour
 {
-    private float scrollSpeed = 8f;
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector2.left * scrollSpeed * Time.deltaTime);
+        transform.Translate(Vector2.left * GameManager.Instance.GetScrollSpeed() * Time.deltaTime);
     }
 }
